7e906f0c753cc3885ae5a7be988cfa1c15290aff
Nathan Kennedy <Nathan630pm@outlook.com>
2021-01-25 20:11:44 -0500

    Made 2 pickers for minutes and seconds

--- WatchTimer WatchKit Extension/ContentView.swift
+++ WatchTimer WatchKit Extension/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var minutes:Int = 1
+    @State private var seconds:Int = 10
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack {
+            Text("Select Time")
+            
+            HStack {
+                Picker(selection: self.$minutes, label: Text("Minutes")){
+                    ForEach((0...60), id: \.self) { minute in
+                        Text("\(minute)").tag(minute)
+                    }
+                }
+                
+                Picker(selection: self.$seconds, label: Text("Seconds")){
+                    ForEach((0...59), id: \.self){ second in
+                        Text("\(second)").tag(second)
+                    }
+                }
+            }
+            
+        }
     }
 }
 
